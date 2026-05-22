a4335b4d7d0a92329155a3d87d5b9a3470cdc48f
Angela Figueroa <figueroa.angela.d@gmail.com>
2026-05-22 18:36:52 -0300

    Agrego metodos Caso List

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,35 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private readonly List<Alumno> _alumnos = new ();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _alumnos.Add (alumno);
+    }
+
+    public List<Alumno> ObtenerAlumnos()
+    {
+        return _alumnos;
+    }
+
+    public Alumno? BuscarAlumno(string nombre)
+    {
+        return _alumnos.FirstOrDefault(alumno => alumno.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void EliminarAlumno(Alumno alumno)
+    {
+        _alumnos.Remove(alumno);
+    }
+
+    public void EliminarAlumnoPorIndice(int index)
+    {
+        if (index < 0 || index >= _alumnos.Count)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index),"Alumno inexistente");
+        }
+
+        _alumnos.RemoveAt(index);
+    }
 }
