1e3b536025809a99c7d72074f11fcb323c9e60a0
Angela Figueroa <figueroa.angela.d@gmail.com>
2026-05-22 18:37:45 -0300

    Agrego metodos Caso Dictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +10,26 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private readonly Dictionary<int, Alumno> _alumnos = new();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _alumnos.Add(alumno.Id, alumno);
+    }
+
+    public Alumno? BuscarAlumno(int legajo)
+    {
+        _alumnos.TryGetValue(legajo, out Alumno? alumno);
+        return alumno;
+    }
+
+    public Dictionary<int, Alumno> ObtenerDiccionario()
+    {
+        return _alumnos;
+    }
+
+    public void EliminarAlumno(int legajo)
+    {
+        _alumnos.Remove(legajo);
+    }
 }
